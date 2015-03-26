83e569571c16e0c1d0ef8e6d63410d2a991818cf
wlzcool <346594796@qq.com>
2015-03-26 16:05:28 +0800

    添加了生成table的pdfdemo

--- ItextSharpDemo/Controllers/PdfDemoController.cs
+++ ItextSharpDemo/Controllers/PdfDemoController.cs
@@ -8,7 +8,7 @@ using it = iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
 using System.Drawing;
-
+using ItextSharpDemo.Models;
 namespace ItextSharpDemo.Controllers
 {
     public class PdfDemoController : Controller
@@ -84,14 +84,58 @@ namespace ItextSharpDemo.Controllers
                     image.ScalePercent(30);
                     document.Add(image);
                 }
-                catch (Exception e) 
+                catch (Exception e)
                 {
-                    document.Add(new it.Paragraph("图片" + item + "不存在")); 
+                    document.Add(new it.Paragraph("图片" + item + "不存在"));
                 }
             }
             document.Close();
             document.Dispose();
             return File(ms.ToArray(), "application/pdf", "ceshi.pdf");
+        }
+        public ActionResult TablePdf()
+        {
+            return View();
+        }
+        public ActionResult GetTablePdf()
+        {
+            it.Font font = new it.Font(BaseFont.CreateFont("C:\\Windows\\Fonts\\simhei.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 10);
+            MemoryStream ms = new MemoryStream();
+            it.Document document = new it.Document();
+            PdfWriter.GetInstance(document, ms);
+            document.Open();
+            document.Add(new it.Paragraph("Yes Master!"));
+            document.Add(new it.Paragraph("其疾如风,其徐如林,侵掠如火,不动如山,难知如阴,动如雷震", font));
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person {Name="wlz",Address="1111"});
+            personList.Add(new Person { Name = "xiaoming", Address = "1112" });
+            personList.Add(new Person { Name = "xiaohong", Address = "1113" });
+            List<NameToColName> nameList = new List<NameToColName>();
+            nameList.Add(new NameToColName { ModelName = "Name", ColName = "姓名" });
+            nameList.Add(new NameToColName { ModelName = "Address", ColName = "地址" });
+            var properties = personList.First().GetType().GetProperties() as System.Reflection.PropertyInfo[];
+            PdfPTable nameTable = new PdfPTable(properties.Length);
+            //创建表头
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var property = properties[i];
+                string colName = (nameList.Where(p => p.ModelName == property.Name).ToList())[0].ColName;
+                nameTable.AddCell(new it.Phrase(colName,font));//注意加上中文字体
+            }
+            
+            personList.ForEach(item =>
+            {                              
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    var property = properties[i];
+                    nameTable.AddCell(new it.Phrase(property.GetValue(item, null).ToString(),font));
+                }
+            });
+            document.Add(nameTable);
+            
+            document.Close();
+            document.Dispose();
+            return File(ms.ToArray(), "application/pdf", "TableDemo.pdf");
         }
         /// <summary>
         /// //对字节数组字符串进行Base64解码并生成图片
@@ -143,5 +187,6 @@ namespace ItextSharpDemo.Controllers
             }
             return ImageStringList;
         }
+
     }
 }
---
+++ ItextSharpDemo/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ItextSharpDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+    public class NameToColName
+    {
+        public string ModelName { get; set; }
+        public string ColName { get; set; }
+    }
+}
